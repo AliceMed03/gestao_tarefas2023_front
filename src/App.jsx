cbc815f66dd292e74bbe6d04797ae1e189cfc37d
AliceMed03 <alicemedeirosds@gmail.com>
2023-11-29 20:57:41 -0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 //import './App.css';
+import { Router } from 'express';
 import cadastrar_tarefa from './components/cadastrar_tarefa';
 import {Routes,Route} from 'react-router-dom';
 const App = () => {
   return(  //tudo que vai aqui no return é o que aparece na aplicação
     <>
-      <Routes>
-        <Route path="/" element={<cadastrar_tarefa/>}/>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<cadastrar_tarefa/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
