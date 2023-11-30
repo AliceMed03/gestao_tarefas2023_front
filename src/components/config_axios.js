61203dd3ccf65beaba7799d74b4742f00ca7ddd4
AliceMed03 <alicemedeirosds@gmail.com>
2023-11-29 21:53:24 -0300

    ajuste para tarefa

--- src/components/config_axios.js
+++ src/components/config_axios.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 //criar uma instância Axios com a URL do back end
 //baseURL é a porta que está rodando o backend
 export const api = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'http://localhost:8081'
 });
