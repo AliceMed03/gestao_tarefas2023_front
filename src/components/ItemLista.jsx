9d16fecc4b3626440e23f51e3d96f6281dd60a02
AliceMed03 <alicemedeirosds@gmail.com>
2023-12-06 20:14:55 -0300

    finish ItemLista

--- src/components/ItemLista.jsx
+++ src/components/ItemLista.jsx
@@ -1,18 +1,16 @@
 import "../css/ItemLista.css";
 //const ItemLista = (props) => { 
 //nocódigo abaixo fiz a desestruturação de props
-const ItemLista = ({id,titulo,autor,ano,preco,foto,excluirClick,alterarClick}) => {
+const ItemLista = ({id,titulo,descricao,status,data_criacao,data_limite,excluirClick,alterarClick}) => {
     return (
         <tr>
             <td>{id}</td>
             <td>{titulo}</td>
-            <td>{autor}</td>
-            <td>{ano}</td>
-            <td class="text-end">
-                {Number(preco).toLocaleString("pt-br", {minimumFractionDigits:2})}
-            </td>
+            <td>{descricao}</td>
+            <td>{status}</td>
+            <td>{data_criacao}</td>
+            <td>{data_limite}</td>
             <td class="text-center">
-                <img src={foto} alt="capa do livro" width="75px"/>
             </td>
             <td class="text-center">
                 <i className="exclui text-danger fw-bold" title="Excluir" onClick={excluirClick}>&#10008;</i>
